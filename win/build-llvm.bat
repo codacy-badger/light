5e9ae400e31d5a45bebff04e62a72a95a706fcb5
Aleix Valls <aleix@lecoati.com>
2017-09-18 14:13:05 +0200

    only recompile LLVM when LIB files are not present

--- win/build-llvm.bat
+++ win/build-llvm.bat
@@ -1,4 +1,14 @@
+@echo off
+
 REM Current LLVM hash -> 65bdf0ae4a87e6992c24f06e2612909952468710
+
+if exist %~dp0..\llvm\Release\lib\LLVMCore.lib (
+	if not "%1"=="force" (
+		echo LIB files already compiled!
+		echo Use parameter "force" to recompile
+		exit /B
+	)
+)
 
 cd llvm
 
@@ -6,5 +16,6 @@ cmake -DLLVM_INCLUDE_TOOLS=OFF -DLLVM_BUILD_TOOLS=OFF -DLLVM_INCLUDE_EXAMPLES=OF
 	-DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Release --build . -Thost=x64
 
 cmake --build . --target all_build --config Release
+) else echo LIB files already compiled
 
 cd ..
