afccd1150dc3ffe46ebed60ceb149f75cd005793
Aleix Valls <aleix@lecoati.com>
2017-10-09 11:30:09 +0200

    remove static LLVM tests

--- main.cpp
+++ main.cpp
@@ -29,170 +29,6 @@ void printVersion (raw_ostream& out) {
 	out << "Light Compiler 0.1.0\n";
 }
 
-Function* makeFunction (Module* module, string name, Type* returnType, std::vector<Type*> params) {
-	FunctionType *functionType = FunctionType::get(returnType, params, false);
-	Function *function = Function::Create(functionType, Function::ExternalLinkage, name.c_str(), module);
-	//function->addFnAttr(Attribute::AttrKind::AlwaysInline);
-	return function;
-}
-
-void std_print (LLVMContext& context, Module* module, IRBuilder<> builder, Value* message) {
-	vector<Type*> putsArgs;
-	putsArgs.push_back(Type::getInt8Ty(context)->getPointerTo());
-	FunctionType *functionType = FunctionType::get(Type::getVoidTy(context), putsArgs, false);
-	Constant* const_func = module->getOrInsertFunction("print", functionType);
-	Function* print_func = cast<Function>(const_func);
-
-	vector<Value*> args { message };
-	builder.CreateCall(print_func, args);
-}
-
-void std_print_i32 (LLVMContext& context, Module* module, IRBuilder<> builder, Value* number) {
-	vector<Type*> putsArgs;
-	putsArgs.push_back(Type::getInt32Ty(context));
-	FunctionType *functionType = FunctionType::get(Type::getVoidTy(context), putsArgs, false);
-	Constant* const_func = module->getOrInsertFunction("print_i32", functionType);
-	Function* print_func = cast<Function>(const_func);
-
-	vector<Value*> args { number };
-	builder.CreateCall(print_func, args);
-}
-
-void std_exit (LLVMContext& context, Module* module, IRBuilder<> builder, int exitCode) {
-	vector<Type*> putsArgs3 { Type::getInt32Ty(context) };
-	Function* ExitProcessfunc = makeFunction(module, "system_exit", Type::getVoidTy(context), putsArgs3);
-	builder.CreateCall(ExitProcessfunc, ConstantInt::get(context, APInt(32, exitCode)));
-	builder.CreateRetVoid();
-}
-
-Module* getIfElseModule (LLVMContext& context, int val) {
-	Module* module = new Module("ifelse", context);
-	IRBuilder<> builder(context);
-
-	Type* Tint32 = Type::getInt32Ty(context);
-	Type* Tvoid = Type::getVoidTy(context);
-
-	vector<Type*> params;
-	Function* mainFunction = makeFunction(module, "main", Tvoid, params);
-	BasicBlock *entryBB = BasicBlock::Create(context, "entry", mainFunction);
-	BasicBlock *thenBB = BasicBlock::Create(context, "if.then", mainFunction);
-	BasicBlock *elseBB = BasicBlock::Create(context, "if.else", mainFunction);
-	BasicBlock *exitBB = BasicBlock::Create(context, "exit", mainFunction);
-
-	builder.SetInsertPoint(entryBB);
-
-	Value* varA = builder.CreateAlloca(Tint32, nullptr, "a");
-	builder.CreateStore(ConstantInt::get(context, APInt(32, 1)), varA);
-	Value* varA2 = builder.CreateLoad(varA, "a2");
-
-	Value* cond = builder.CreateICmpSGT(varA2, ConstantInt::get(context, APInt(32, 0)));
-	builder.CreateCondBr(cond, thenBB, elseBB);
-
-	builder.SetInsertPoint(thenBB);
-	std_print(context, module, builder, builder.CreateGlobalStringPtr("[ifelse] then block reached!\n"));
-	builder.CreateStore(ConstantInt::get(context, APInt(32, 42)), varA);
-	builder.CreateBr(exitBB);
-
-	builder.SetInsertPoint(elseBB);
-	std_print(context, module, builder, builder.CreateGlobalStringPtr("[ifelse] else block reached!\n"));
-	builder.CreateStore(ConstantInt::get(context, APInt(32, -42)), varA);
-	builder.CreateBr(exitBB);
-
-	builder.SetInsertPoint(exitBB);
-	std_print(context, module, builder, builder.CreateGlobalStringPtr("[ifelse] value -> "));
-	std_print_i32(context, module, builder, builder.CreateLoad(varA, "a2"));
-	std_print(context, module, builder, builder.CreateGlobalStringPtr("\n"));
-	std_exit(context, module, builder, 0);
-
-	return module;
-}
-
-Module* getForModule (LLVMContext& context, int val) {
-	Module* module = new Module("for", context);
-	IRBuilder<> builder(context);
-
-	Type* Tint32 = Type::getInt32Ty(context);
-	Type* Tint8 = Type::getInt8Ty(context);
-	Type* Tvoid = Type::getVoidTy(context);
-
-	vector<Type*> params;
-	Function* mainFunction = makeFunction(module, "main", Tvoid, params);
-	BasicBlock *entryBB = BasicBlock::Create(context, "entry", mainFunction);
-	BasicBlock *condBB = BasicBlock::Create(context, "for.cond", mainFunction);
-	BasicBlock *bodyBB = BasicBlock::Create(context, "for.body", mainFunction);
-	BasicBlock *incBB = BasicBlock::Create(context, "for.inc", mainFunction);
-	BasicBlock *exitBB = BasicBlock::Create(context, "exit", mainFunction);
-
-	builder.SetInsertPoint(entryBB);
-	Value* varA = builder.CreateAlloca(Tint32, nullptr, "a");
-	Value* varI = builder.CreateAlloca(Tint8, nullptr, "i");
-	builder.CreateStore(ConstantInt::get(context, APInt(32, 0)), varA);
-	builder.CreateStore(ConstantInt::get(context, APInt(8, 0)), varI);
-	builder.CreateBr(condBB);
-
-	builder.SetInsertPoint(condBB);
-	Value* varIv = builder.CreateLoad(varI, "i2");
-	Value* cond = builder.CreateICmpSLT(varIv, ConstantInt::get(context, APInt(8, val)));
-	builder.CreateCondBr(cond, bodyBB, exitBB);
-
-	builder.SetInsertPoint(bodyBB);
-	Value* varAv = builder.CreateLoad(varA, "a2");
-	Value* addA = builder.CreateAdd(varAv, ConstantInt::get(context, APInt(32, 2)), "add");
-	std_print(context, module, builder, builder.CreateGlobalStringPtr("[for] "));
-	std_print_i32(context, module, builder, builder.CreateLoad(varI, "i"));
-	std_print(context, module, builder, builder.CreateGlobalStringPtr(" -> "));
-	std_print_i32(context, module, builder, builder.CreateLoad(varA, "a"));
-	std_print(context, module, builder, builder.CreateGlobalStringPtr("\n"));
-	builder.CreateStore(addA, varA);
-	builder.CreateBr(incBB);
-
-	builder.SetInsertPoint(incBB);
-	varIv = builder.CreateLoad(varI, "i2");
-	Value* incI = builder.CreateAdd(varIv, ConstantInt::get(context, APInt(32, 1)), "inc");
-	builder.CreateStore(incI, varI);
-	builder.CreateBr(condBB);
-
-	builder.SetInsertPoint(exitBB);
-	std_exit(context, module, builder, 0);
-
-	return module;
-}
-
-Module* getStructModule (LLVMContext& context, int number, const char* message) {
-	Module* module = new Module("struct", context);
-
-	Type* Tvoid = Type::getVoidTy(context);
-	Type* Ti32 = Type::getInt32Ty(context);
-	Type* Ti8 = Type::getInt8Ty(context);
-
-	vector<Type*> params;
-	Function* mainFunction = makeFunction(module, "main", Tvoid, params);
-	BasicBlock* block = BasicBlock::Create(context, "entry", mainFunction);
-	IRBuilder<> builder(block);
-
-	StructType::create("struct._a", Ti32, Ti8->getPointerTo());
-	Type* _a = module->getTypeByName("struct._a");
-	AllocaInst* varI32 = builder.CreateAlloca(_a, nullptr, "qwe");
-	Value* structV1 = builder.CreateStructGEP(_a, varI32, 0);
-	Value* structV2 = builder.CreateStructGEP(_a, varI32, 1);
-
-	builder.CreateStore(ConstantInt::get(context, APInt(32, number)), structV1);
-	builder.CreateStore(builder.CreateGlobalStringPtr(message), structV2);
-
-	Value* newLine = builder.CreateGlobalStringPtr("\n");
-	Value* label = builder.CreateGlobalStringPtr("[struct] ");
-	std_print(context, module, builder, label);
-	std_print_i32(context, module, builder, builder.CreateLoad(structV1, "tmp"));
-	std_print(context, module, builder, newLine);
-	std_print(context, module, builder, label);
-	std_print(context, module, builder, builder.CreateLoad(structV2, "tmp"));
-
-	std_exit(context, module, builder, 0);
-
-	verifyModule(*module);
-	return module;
-}
-
 double clockStop (clock_t start) {
 	return (clock() - start) / static_cast<double>(CLOCKS_PER_SEC);
 }
@@ -204,9 +40,7 @@ int main (int argc, char** argv) {
 
 	std::cout << std::fixed << std::setprecision(3);
 
-	LLVMContext GlobalContext;
-	LLVMBackend* backend = new LLVMBackend();
-
+	auto backend = new LLVMBackend();
 	for (auto &filename : InputFilenames) {
 		auto parser = new Parser(filename.c_str());
 
@@ -224,23 +58,6 @@ int main (int argc, char** argv) {
 			std::cout << "TOTAL " << clockStop(total) << "s" << std::endl;
 		}
 	}
-
-	Module* module = getIfElseModule(GlobalContext, 12000);
-	//module->print(outs(), nullptr);
-	auto moduleName = "test\\" + module->getModuleIdentifier() + ".obj";
-	backend->writeObj(module, moduleName.c_str());
-
-	module = getForModule(GlobalContext, 5);
-	//module->print(outs(), nullptr);
-	moduleName = "test\\" + module->getModuleIdentifier() + ".obj";
-	backend->writeObj(module, moduleName.c_str());
-
-	module = getStructModule(GlobalContext, -42, "Ima structooo\n");
-	//module->print(outs(), nullptr);
-	moduleName = "test\\" + module->getModuleIdentifier() + ".obj";
-	backend->writeObj(module, moduleName.c_str());
-
-	delete module;
 
 	return 0;
 }
--- win/test.bat
+++ win/test.bat
@@ -1,22 +1,7 @@
 @echo off
 
-bin\light -o test\test.obj examples\func.li
-
-link /nologo /ENTRY:main /SUBSYSTEM:CONSOLE /OUT:".\test\ifelse.exe" test\ifelse.obj ^
- 	build\std_li.lib kernel32.lib
-
-link /nologo /ENTRY:main /SUBSYSTEM:CONSOLE /OUT:".\test\for.exe" test\for.obj ^
- 	build\std_li.lib kernel32.lib
-
-link /nologo /ENTRY:main /SUBSYSTEM:CONSOLE /OUT:".\test\struct.exe" test\struct.obj ^
- 	build\std_li.lib kernel32.lib
+bin\light -o build\test.obj examples\func.li
 
 echo.
-echo --- TESTS ---
-test\ifelse
-test\for
-test\struct
-
-echo.
-link /nologo /ENTRY:main /OUT:"build\test.exe" test/test.obj kernel32.lib build\std_li.lib
-build\test
+link /nologo /ENTRY:main /OUT:"bin\test.exe" build/test.obj kernel32.lib build\std_li.lib
+bin\test
