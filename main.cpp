c17bc58661b3236219315713a3c28653b75d339b
Aleix Valls <aleix@lecoati.com>
2017-10-09 15:07:04 +0200

    remove llvm imports from main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
-#include "llvm/Support/CommandLine.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Attributes.h"
-#include "llvm/IR/CallingConv.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/Support/raw_ostream.h"
-
 #include <iomanip>
 
+#include "llvm/Support/CommandLine.h"
 #include "parser/parser.cpp"
 #include "parser/printer.cpp"
 #include "back/llvm/llvm_pipe.cpp"
