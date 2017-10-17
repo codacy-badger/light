31da025c31b4531d73f14214d8e258c99bb1d6d0
Aleix Valls <aleix@lecoati.com>
2017-10-17 07:32:32 +0200

    add incremental compilation files to gitignore

--- win/build.bat
+++ win/build.bat
@@ -5,7 +5,7 @@ pushd build
 set "SOURCES="
 for /R "../src" %%i in (*.cpp) do @call set SOURCES=%%SOURCES%% "%%i"
 cl /nologo /Od /c /MD /Zi /Gm -I"../include" %LLVMCLFlags% %SOURCES%
- set "SOURCES="
+set "SOURCES="
 
 link /nologo /INCREMENTAL /ENTRY:mainCRTStartup ^
     /OUT:"..\bin\light.exe" /DEBUG %LLVMLibs% ^
