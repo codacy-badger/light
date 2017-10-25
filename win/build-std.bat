fdef748cdbaf8e7308f19b2f403b7962abb27d82
Aleix Valls <aleix@lecoati.com>
2017-10-25 15:15:45 +0200

    build DLL with std C code

--- win/build-std.bat
+++ win/build-std.bat
@@ -6,7 +6,10 @@ for /r %%i in (win\std\*.c) do (
 
 set "STD_OBJS="
 for %%i in (*.obj) do @call set STD_OBJS=%%STD_OBJS%% "%%i"
+
 lib /nologo /OUT:".\build\std_li.lib" %STD_OBJS% kernel32.lib
+link /nologo /DLL /OUT:".\build\std_li.dll" %STD_OBJS% kernel32.lib
+
 set "STD_OBJS="
 
 del *.obj
