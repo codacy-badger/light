abbe1afe372eb23f1cfedc135caae7d320889144
Aleix Valls <aleix@lecoati.com>
2017-11-16 08:59:41 +0100

    clone all submodules on project setup

--- win/setup.bat
+++ win/setup.bat
@@ -1,4 +1,7 @@
 @echo off
+
+git submodule update --init --recursive
+
 SET CurrentDir=%CD%
 
 if "%VS150COMNTOOLS%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
