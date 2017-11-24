658d400d551c33770f1a67ff03b646027a71f90d
avalls <aleixvalls14@gmail.com>
2017-11-24 18:50:00 +0100

    simplify code example

--- src/bytecode/pipe/bytecode_generator.cpp
+++ src/bytecode/pipe/bytecode_generator.cpp
@@ -220,6 +220,13 @@ void Bytecode_Generator::gen (Ast_Function* fn) {
 		this->current_register = 0;
 		this->bytecode = &fn->bytecode;
 		this->gen(fn->scope);
+		if (fn->type->return_type == Light_Compiler::inst->type_def_void) {
+			auto last_inst = this->bytecode->back();
+			if (last_inst->bytecode != BYTECODE_RETURN) {
+			    auto inst2 = new Inst_Return();
+			    this->bytecode->push_back(copy_location_info(inst2, fn->scope));
+			}
+		}
 	}
 }
 
