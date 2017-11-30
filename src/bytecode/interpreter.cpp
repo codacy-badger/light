c632acbcd78500a5184ab58ae076d1186ea0d322
Aleix Valls <aleix@lecoati.com>
2017-11-30 10:36:05 +0100

    simplify bytecode interpreter cast

--- src/bytecode/interpreter.cpp
+++ src/bytecode/interpreter.cpp
@@ -8,7 +8,7 @@
 
 #include "compiler.hpp"
 
-#define DEBUG true
+#define DEBUG false
 
 #define INST_OFFSET(offset) buffer[offset]
 
@@ -53,138 +53,63 @@ void Bytecode_Interpreter::run (Ast_Function* func) {
 	this->stack_index = _tmp;
 }
 
-template<typename F, typename T>
-T s_cast (F value) {
-	return static_cast<T>(value);
+template<typename F>
+void _cast_signed (void* ptr, size_t size_to) {
+	F value;
+	memcpy(&value, ptr, sizeof(F));
+	// We have to do this, since the cast for signed numbers
+	// has to mantain the sign bit,
+	// TODO: maybe we could simplify this by using bitmasks?
+	switch (size_to) {
+		case 1: {
+			auto new_value = static_cast<int8_t>(value);
+			memcpy(ptr, &new_value, size_to);
+			break;
+		}
+		case 2: {
+			auto new_value = static_cast<int16_t>(value);
+			memcpy(ptr, &new_value, size_to);
+			break;
+		}
+		case 4: {
+			auto new_value = static_cast<int32_t>(value);
+			memcpy(ptr, &new_value, size_to);
+			break;
+		}
+		case 8: {
+			auto new_value = static_cast<int64_t>(value);
+			memcpy(ptr, &new_value, size_to);
+			break;
+		}
+	}
+}
+
+template<typename F>
+void _cast_unsigned (void* ptr, size_t size_to) {
+	F value;
+	memcpy(&value, ptr, sizeof(F));
+	auto new_value = static_cast<size_t>(value);
+	memcpy(ptr, &new_value, size_to);
 }
 
 void bytecode_cast(void* reg_ptr, uint8_t type_from, uint8_t type_to) {
 	auto size_from = bytecode_get_size(type_from);
 	auto size_to = bytecode_get_size(type_to);
 	auto sign_from = bytecode_has_sign(type_from);
-	auto sign_to = bytecode_has_sign(type_to);
 
 	if (sign_from) {
 		switch (size_from) {
-			case 1: {
-				int8_t value;
-				memcpy(&value, reg_ptr, size_from);
-				switch (size_to) {
-					case 2: {
-						int16_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 4: {
-						int32_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 8: {
-						int64_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-				}
-				break;
-			}
-			case 2: {
-				int16_t value;
-				memcpy(&value, reg_ptr, size_from);
-				switch (size_to) {
-					case 1: {
-						int8_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 4: {
-						int32_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 8: {
-						int64_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-				}
-				break;
-			}
-			case 4: {
-				int32_t value;
-				memcpy(&value, reg_ptr, size_from);
-				switch (size_to) {
-					case 1: {
-						int8_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 2: {
-						int16_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 8: {
-						int64_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-				}
-				break;
-			}
-			case 8: {
-				int64_t value;
-				memcpy(&value, reg_ptr, size_from);
-				switch (size_to) {
-					case 1: {
-						int8_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 2: {
-						int16_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-					case 4: {
-						int32_t value2 = value;
-						memcpy(reg_ptr, &value2, size_to);
-						break;
-					}
-				}
-				break;
-			}
+			case 1: _cast_signed<int8_t>(reg_ptr, size_to); break;
+			case 2: _cast_signed<int16_t>(reg_ptr, size_to); break;
+			case 4: _cast_signed<int32_t>(reg_ptr, size_to); break;
+			case 8: _cast_signed<int64_t>(reg_ptr, size_to); break;
 		}
 	} else {
 		switch (size_from) {
-			case 1: {
-				uint8_t value;
-				memcpy(&value, reg_ptr, 1);
-				size_t _tmp = value;
-				memcpy(reg_ptr, &_tmp, size_to);
-				break;
-			}
-			case 2: {
-				uint16_t value;
-				memcpy(&value, reg_ptr, 2);
-				size_t _tmp = value;
-				memcpy(reg_ptr, &_tmp, size_to);
-				break;
-			}
-			case 4: {
-				uint32_t value;
-				memcpy(&value, reg_ptr, 4);
-				size_t _tmp = value;
-				memcpy(reg_ptr, &_tmp, size_to);
-				break;
-			}
-			case 8: {
-				uint64_t value;
-				memcpy(&value, reg_ptr, 8);
-				size_t _tmp = value;
-				memcpy(reg_ptr, &_tmp, size_to);
-				break;
-			}
+			case 1: _cast_unsigned<uint8_t>(reg_ptr, size_to); break;
+			case 2: _cast_unsigned<uint16_t>(reg_ptr, size_to); break;
+			case 4: _cast_unsigned<uint32_t>(reg_ptr, size_to); break;
+			case 8: _cast_unsigned<uint64_t>(reg_ptr, size_to); break;
 		}
 	}
 }
