7e89e15f9a3312a1efd60b976fac689d16d6713b
Aleix Valls <ava@novicell.es>
2018-12-03 12:08:44 +0100

    remove incremental build option from build file

--- platform/win/build.bat
+++ platform/win/build.bat
@@ -10,7 +10,7 @@ for /R "../src" %%i in (*.cpp) do @call set SOURCES=%%SOURCES%% "%%i"
 for /R "../platform/win/src" %%i in (*.cpp) do @call set SOURCES=%%SOURCES%% "%%i"
 
 REM to dissable all assertions add "/DNDEBUG" to the flags
-set FLAGS=/nologo /Od /c /MD /Zi /Gm /W4 /WX /EHsc
+set FLAGS=/nologo /Od /c /MD /Zi /W4 /WX /EHsc
 cl %FLAGS% -I"../include" -I"../dyncall" %SOURCES%
 
 link /nologo /INCREMENTAL /ENTRY:mainCRTStartup /subsystem:console ^
