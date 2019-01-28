48d95260505ecd627295b0654f8f45180547bd85
Aleix Valls Pérez <aleixvalls14@gmail.com>
2019-01-28 20:32:41 +0100

    improve ast printer

--- include/ast/printer.hpp
+++ include/ast/printer.hpp
@@ -2,61 +2,82 @@
 
 #include <cstdarg>
 
+#define PRINT_TABS for (size_t i = current_tabs; i > 0; i--) printf("    ");
+
 struct Ast_Printer {
-    static void print(Ast_Scope* scope, uint8_t tabs = 0) {
-        print(tabs, "{");
-        for (auto stm : scope->statements) {
-            print(stm, tabs + 1);
+    size_t current_tabs;
+
+    Ast_Printer (size_t default_tab_level = 0) { this->current_tabs = default_tab_level; }
+
+    void print (Ast_Arguments* args) {
+        print("(");
+        if (args->unnamed.size() > 0) {
+            print(args->unnamed[0]);
+            for (int i = 1; i < args->unnamed.size(); i++) {
+                print(", ");
+                print(args->unnamed[i]);
+            }
         }
-        print(tabs, "}\n");
+        print(")");
     }
 
-    static void print(Ast_Statement* stm, uint8_t tabs = 0) {
+    void print(Ast_Statement* stm) {
         switch (stm->stm_type) {
 			case AST_STATEMENT_SCOPE: {
-				print(reinterpret_cast<Ast_Scope*>(stm), tabs);
+				print(reinterpret_cast<Ast_Scope*>(stm));
 				break;
 			}
 			case AST_STATEMENT_IF: {
-				print(reinterpret_cast<Ast_If*>(stm), tabs);
+				print(reinterpret_cast<Ast_If*>(stm));
 				break;
 			}
 			case AST_STATEMENT_WHILE: {
-				print(reinterpret_cast<Ast_While*>(stm), tabs);
+				print(reinterpret_cast<Ast_While*>(stm));
 				break;
 			}
 			case AST_STATEMENT_DECLARATION: {
-				print(reinterpret_cast<Ast_Declaration*>(stm), tabs);
+				print(reinterpret_cast<Ast_Declaration*>(stm));
 				break;
 			}
 			case AST_STATEMENT_RETURN: {
-				print(reinterpret_cast<Ast_Return*>(stm), tabs);
+				print(reinterpret_cast<Ast_Return*>(stm));
 				break;
 			}
 			case AST_STATEMENT_BREAK: {
-				print(reinterpret_cast<Ast_Break*>(stm), tabs);
+				print(reinterpret_cast<Ast_Break*>(stm));
 				break;
 			}
 			case AST_STATEMENT_IMPORT: {
-				print(reinterpret_cast<Ast_Import*>(stm), tabs);
+				print(reinterpret_cast<Ast_Import*>(stm));
 				break;
 			}
 			case AST_STATEMENT_FOREIGN: {
-				print(reinterpret_cast<Ast_Foreign*>(stm), tabs);
+				print(reinterpret_cast<Ast_Foreign*>(stm));
 				break;
 			}
 			case AST_STATEMENT_EXPRESSION: {
-                print(tabs, "");
 				print(reinterpret_cast<Ast_Expression*>(stm));
-                print("\n");
 				break;
 			}
 			default: break;
 		}
     }
 
-	static void print(Ast_Declaration* decl, uint8_t tabs) {
-        print(tabs, "%s ", decl->name);
+    void print(Ast_Scope* scope) {
+        printf("{\n");
+        this->current_tabs += 1;
+        for (auto stm : scope->statements) {
+            PRINT_TABS;
+            print(stm);
+            printf("\n");
+        }
+        this->current_tabs -= 1;
+        PRINT_TABS;
+        printf("}\n");
+    }
+
+	void print(Ast_Declaration* decl) {
+        printf("%s ", decl->name);
 
         if (decl->type) {
             print(": ");
@@ -69,21 +90,18 @@ struct Ast_Printer {
             } else print("= ");
             print(decl->expression);
         }
-
-        print(";\n");
 	}
 
-	static void print(Ast_Return* ret, uint8_t tabs) {
-        print(tabs, "return");
+	void print(Ast_Return* ret) {
+        print("return");
         if (ret->expression) {
             print(" ");
-            print(ret->expression, tabs);
+            print(ret->expression);
         }
-        print(";\n");
 	}
 
-	static void print(Ast_If* _if, uint8_t tabs) {
-        print(tabs, "if (");
+	void print(Ast_If* _if) {
+        print("if (");
         print(_if->condition);
         print(") ");
 		print(_if->then_scope);
@@ -93,88 +111,313 @@ struct Ast_Printer {
         }
 	}
 
-	static void print(Ast_While* _while, uint8_t tabs) {
-        print(tabs, "while (");
+	void print(Ast_While* _while) {
+        print("while (");
         print(_while->condition);
         print(") ");
 		print(_while->scope);
 	}
 
-	static void print(Ast_Import* import, uint8_t tabs) {
-        print(tabs, "import");
+	void print(Ast_Import* import) {
+        print("import");
         if (import->include) print("!");
-        print(" \"%s\"\n", import->path);
+        print(" \"%s\"", import->path);
     }
 
-	static void print(Ast_Foreign* foreign, uint8_t tabs) {
-        print(tabs, "foreign ");
+	void print(Ast_Foreign* foreign) {
+        print("foreign ");
         if (foreign->module_name) print("%s ", foreign->module_name);
         if (foreign->function_name) print("%s ", foreign->function_name);
         print("{\n");
         for (auto decl : foreign->declarations) {
-            print(decl, tabs + 1);
+            print(decl + 1);
         }
-        print(tabs, "}\n");
+        print("}");
     }
 
-	static void print(Ast_Break*, uint8_t) { /* empty */ }
+	void print(Ast_Break*) {
+        print("break");
+    }
 
-	static void print(Ast_Expression*) {
-        print("<EXP>");
-		/*switch (exp->exp_type) {
+	void print(Ast_Expression* exp) {
+		switch (exp->exp_type) {
 			case AST_EXPRESSION_RUN: {
-				this->ast_handle(reinterpret_cast<Ast_Run*>(exp));
+				print(reinterpret_cast<Ast_Run*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_FUNCTION: {
-				this->ast_handle(reinterpret_cast<Ast_Function*>(exp));
+				print(reinterpret_cast<Ast_Function*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_CALL: {
-				this->ast_handle(reinterpret_cast<Ast_Function_Call*>(exp));
+				print(reinterpret_cast<Ast_Function_Call*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_BINARY: {
-				this->ast_handle(reinterpret_cast<Ast_Binary*>(exp));
+				print(reinterpret_cast<Ast_Binary*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_UNARY: {
-				this->ast_handle(reinterpret_cast<Ast_Unary*>(exp));
+				print(reinterpret_cast<Ast_Unary*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_CAST: {
-				this->ast_handle(reinterpret_cast<Ast_Cast*>(exp));
+				print(reinterpret_cast<Ast_Cast*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_IDENT: {
-				this->ast_handle(reinterpret_cast<Ast_Ident*>(exp));
+				print(reinterpret_cast<Ast_Ident*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_LITERAL: {
-				this->ast_handle(reinterpret_cast<Ast_Literal*>(exp));
+				print(reinterpret_cast<Ast_Literal*>(exp));
 				break;
 			}
 			case AST_EXPRESSION_TYPE_INSTANCE: {
-				this->ast_handle(reinterpret_cast<Ast_Type_Instance*>(exp));
+				print(reinterpret_cast<Ast_Type_Instance*>(exp));
 				break;
 			}
-		}*/
+		}
 	}
 
-    static void print(Ast_Arguments*, uint8_t) { /* empty */ }
-
-    static void print(uint8_t tabs, const char* format, ...) {
-        va_list argptr;
-        va_start(argptr, format);
-        while (tabs > 0) {
-            printf("    ");
-            tabs -= 1;
-        }
-        vprintf(format, argptr);
-        va_end(argptr);
+    void print(Ast_Run* run) {
+        print("#(");
+        print(run->expression);
+        print(")");
     }
 
-    static void print(const char* format, ...) {
+    void print(Ast_Function* func) {
+        print(func->type);
+        print(" ");
+        print(func->scope);
+    }
+
+    void print(Ast_Function_Call* call) {
+        print(call->func);
+        print(call->arguments);
+    }
+
+    void print(Ast_Unary* unary) {
+        switch (unary->unary_op) {
+            case AST_UNARY_DEREFERENCE: {
+                printf("&");
+                break;
+            }
+        	case AST_UNARY_REFERENCE: {
+                printf("*");
+                break;
+            }
+        	case AST_UNARY_NOT: {
+                printf("!");
+                break;
+            }
+        	case AST_UNARY_NEGATE: {
+                printf("-");
+                break;
+            }
+        }
+        print(unary->exp);
+    }
+
+    void print(Ast_Binary* binary) {
+        print(binary->lhs);
+        switch (binary->binary_op) {
+            case AST_BINARY_ASSIGN: {
+                print(" = ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_ATTRIBUTE: {
+                print(".");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_SUBSCRIPT: {
+                print("[");
+                print(binary->rhs);
+                print("]");
+                break;
+            }
+        	case AST_BINARY_LOGICAL_AND: {
+                print(" && ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_LOGICAL_OR: {
+                print(" || ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_ADD: {
+                print(" + ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_SUB: {
+                print(" - ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_MUL: {
+                print(" * ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_DIV: {
+                print(" / ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_REM: {
+                print(" %% ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_BITWISE_AND: {
+                print(" & ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_BITWISE_OR: {
+                print(" | ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_BITWISE_XOR: {
+                print(" ^ ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_BITWISE_RIGHT_SHIFT: {
+                print(" >> ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_BITWISE_LEFT_SHIFT: {
+                print(" << ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_EQ: {
+                print(" == ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_NEQ: {
+                print(" != ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_LT: {
+                print(" < ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_LTE: {
+                print(" <= ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_GT: {
+                print(" > ");
+                print(binary->rhs);
+                break;
+            }
+        	case AST_BINARY_GTE: {
+                print(" >= ");
+                print(binary->rhs);
+                break;
+            }
+        }
+    }
+
+    void print(Ast_Cast* cast) {
+        print("cast(");
+        print(cast->cast_to);
+        print(") ");
+        print(cast->value);
+    }
+
+    void print(Ast_Ident* ident) {
+        print(ident->name);
+    }
+
+    void print(Ast_Literal* lit) {
+        switch (lit->literal_type) {
+            case AST_LITERAL_SIGNED_INT: {
+                print("%lld", lit->int_value);
+                break;
+            }
+        	case AST_LITERAL_UNSIGNED_INT: {
+                print("%llu", lit->uint_value);
+                break;
+            }
+        	case AST_LITERAL_DECIMAL: {
+                print("%lf", lit->int_value);
+                break;
+            }
+        	case AST_LITERAL_STRING: {
+                print(lit->string_value);
+                break;
+            }
+        }
+    }
+
+    void print(Ast_Type_Instance* type) {
+        switch (type->typedef_type) {
+        	case AST_TYPEDEF_FUNCTION : {
+                print(reinterpret_cast<Ast_Function_Type*>(type));
+                break;
+            }
+        	case AST_TYPEDEF_STRUCT : {
+                print(reinterpret_cast<Ast_Struct_Type*>(type));
+                break;
+            }
+        	case AST_TYPEDEF_POINTER : {
+                print(reinterpret_cast<Ast_Pointer_Type*>(type));
+                break;
+            }
+        	case AST_TYPEDEF_ARRAY : {
+                print(reinterpret_cast<Ast_Array_Type*>(type));
+                break;
+            }
+        }
+    }
+
+    void print(Ast_Function_Type* func_type) {
+        print("fn");
+        if (func_type->arg_decls.size() > 0) {
+            print(" (");
+            print(func_type->arg_decls[0]);
+            for (int i = 1; i < func_type->arg_decls.size(); i++) {
+                print(", ");
+                print(func_type->arg_decls[i]);
+            }
+            print(")");
+        }
+        if (func_type->ret_type) {
+            print(" -> ");
+            print(func_type->ret_type);
+        }
+    }
+
+    void print(Ast_Struct_Type* struct_type) {
+        print(struct_type->name);
+    }
+
+    void print(Ast_Pointer_Type* ptr_type) {
+        print("*");
+        print(ptr_type->base);
+    }
+
+    void print(Ast_Array_Type* array_type) {
+        print("[");
+        print(array_type->length);
+        print("] ");
+        print(array_type->base);
+    }
+
+    void print(const char* format, ...) {
         va_list argptr;
         va_start(argptr, format);
         vprintf(format, argptr);
--- include/steps/imp/print_step.hpp
+++ include/steps/imp/print_step.hpp
@@ -6,9 +6,13 @@
 #include "ast/printer.hpp"
 
 struct Print_Step : Step<>, Ast_Navigator {
+    Ast_Printer* printer = new Ast_Printer();
+
     Print_Step () : Step("Ast Printer") { /* empty */ }
 
     void run (Ast_Statement* stm) {
-        Ast_Printer::print(stm, 1);
+        printf("\n");
+        this->printer->print(stm);
+        printf("\n");
     }
 };
