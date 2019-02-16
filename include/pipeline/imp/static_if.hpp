e6c68d9647e3158ac964b6c72f09c61f0e647b88
Aleix Valls Pérez <aleixvalls14@gmail.com>
2019-02-16 13:46:26 +0100

    internal static ifs now work as expected

--- include/pipeline/imp/static_if.hpp
+++ include/pipeline/imp/static_if.hpp
@@ -4,17 +4,27 @@
 #include "utils/ast_navigator.hpp"
 
 struct Static_If : Compiler_Pipe<Ast_Statement*>, Ast_Navigator {
+    Ast_Scope* current_global_scope = NULL;
     Ast_Scope* current_scope = NULL;
+
     Async_Queue<Ast_Statement*>* flow_back = NULL;
+    bool has_generated_new_internal_code = false;
 
     Static_If(Async_Queue<Ast_Statement*>* flow_back) : Compiler_Pipe("Static If") {
         this->flow_back = flow_back;
     }
 
     void handle (Ast_Statement* global_statement) {
+        this->current_global_scope = global_statement->parent_scope;
         this->current_scope = global_statement->parent_scope;
+        this->has_generated_new_internal_code = false;
         Ast_Navigator::ast_handle(global_statement);
-        this->push_out(global_statement);
+
+        if (global_statement->stm_type != AST_STATEMENT_STATIC_IF) {
+            if (this->has_generated_new_internal_code) {
+                flow_back->push(global_statement);
+            } else this->push_out(global_statement);
+        }
     }
 
     void ast_handle (Ast_Static_If* static_if) {
@@ -26,12 +36,29 @@ struct Static_If : Compiler_Pipe<Ast_Statement*>, Ast_Navigator {
         } else if (_if->else_body) {
             this->resolve_static_if(static_if, _if->else_body);
         }
+
+        if (this->current_scope != this->current_global_scope) {
+            this->current_scope->remove(static_if);
+        }
     }
 
     void resolve_static_if (Ast_Static_If* static_if, Ast_Scope* scope_to_merge) {
-        static_if->parent_scope->add(static_if->parent_scope->find(static_if), scope_to_merge);
-        for (auto stm : scope_to_merge->statements) {
-            flow_back->push(stm);
+        auto static_if_location = static_if->parent_scope->find(static_if);
+        static_if->parent_scope->add(static_if_location, scope_to_merge);
+
+        if (this->current_scope == this->current_global_scope) {
+            for (auto stm : scope_to_merge->statements) {
+                flow_back->push(stm);
+            }
+        } else this->has_generated_new_internal_code = true;
+    }
+
+    void ast_handle (Ast_Declaration* decl) {
+        // INFO: we don't want to resolve static ifs for declaration types, since
+        // those types could be values of other global declaration and we would
+        // be falsely responsible of resolving those ifs.
+        if (decl->expression) {
+            Ast_Navigator::ast_handle(decl->expression);
         }
     }
 
