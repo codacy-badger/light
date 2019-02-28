236cc905371ad48dadd33dabf6bb08a9dcb1d59e
Aleix Valls <ava@novicell.es>
2019-02-28 16:58:40 +0100

    build now using clang-cl & lld-link

--- os/win32/build.bat
+++ os/win32/build.bat
@@ -7,15 +7,13 @@ if not exist bin mkdir bin
 
 pushd build
 
-set "SOURCES="
-for /R "../src" %%i in (*.cpp) do @call set SOURCES=%%SOURCES%% "%%i"
-for /R "../os/win32/src" %%i in (*.cpp) do @call set SOURCES=%%SOURCES%% "%%i"
+REM compile using clang-cl
+clang-cl /nologo /Od /c /MD /Zi /W4 /EHsc /DCUSTOM_DEBUG ^
+    -I"../include" -I"../dyncall" ^
+    ..\src\*.cpp ..\os\win32\src\*.cpp
 
-REM to dissable all assertions remove the "/DCUSTOM_DEBUG" flag
-set FLAGS=/nologo /Od /c /MD /MP /Zi /W4 /WX /EHsc /DCUSTOM_DEBUG
-cl %FLAGS% -I"../include" -I"../dyncall" %SOURCES%
-
-link /nologo /INCREMENTAL /ENTRY:mainCRTStartup /subsystem:console ^
+REM link using lld-link
+lld-link /nologo /INCREMENTAL /ENTRY:mainCRTStartup /subsystem:console ^
     /OUT:"..\bin\light.exe" /DEBUG /NODEFAULTLIB:LIBCMT *.obj ^
     ..\dyncall\dyncall\libdyncall_s.lib user32.lib
 
